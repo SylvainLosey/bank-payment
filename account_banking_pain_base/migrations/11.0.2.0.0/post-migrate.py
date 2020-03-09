f53a9b7d8adb50e0d733f8119bde80f72d6aed21
Sylvain <sylvainlosey@gmail.com>
2020-03-09 09:05:46 +0100

    Update migration script
    - Odoo will compute charge_bearer automatically as it is a new field
    - Correcting data to SLEV should not be done here

--- account_banking_pain_base/migrations/11.0.2.0.0/post-migrate.py
+++ account_banking_pain_base/migrations/11.0.2.0.0/post-migrate.py
@@ -12,19 +12,9 @@ def migrate(env, version):
         return
     cr = env.cr
 
-    # update charge_bearer of account_payment_lines
-    plines = env['account.payment.line'].search([])
-    for pline in plines:
-        pline.compute_charge_bearer()
-
     # update charge_bearer for all bank_payment_line
     cr.execute(
         "UPDATE bank_payment_line as l "
         "SET charge_bearer = ("
         "SELECT charge_bearer FROM account_payment_order as o "
         "WHERE o.id = l.order_id)")
-    # correct charge_bearer for sepa bank_payment_line (must be 'SLEV')
-    blines = env['bank.payment.line'].search([])
-    for bline in blines:
-        if bline.sepa:
-            bline.charge_bearer = 'SLEV'
